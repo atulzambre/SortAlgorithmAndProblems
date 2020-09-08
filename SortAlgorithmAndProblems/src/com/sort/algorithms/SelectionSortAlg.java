144b577170013b63098e521154bdbc5e88bdcaec
atulzambre <atul.zambre123@gmail.com>
2020-09-08 15:06:14 +0530

    selection sort algorithm implemented

--- SortAlgorithmAndProblems/src/com/sort/algorithms/SelectionSortAlg.java
+++ SortAlgorithmAndProblems/src/com/sort/algorithms/SelectionSortAlg.java
@@ -1,4 +1,7 @@
 package com.sort.algorithms;
+
+import java.util.Arrays;
+
 /*
 The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.
  The algorithm maintains two subarrays in a given array.
@@ -14,9 +17,22 @@ public class SelectionSortAlg {
         int arr[]={3,7,6,1,2,5,4};
 
         selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void selectionSort(int[] arr) {
+
+        for (int i=0;i<arr.length;i++){
+          int min=i;
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
         
     }
 }
